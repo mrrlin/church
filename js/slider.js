ceeaf2c54bfad72c2d4b6856906d8f45e30d627a
mRkapustkin <kapm1821@gmail.com>
2024-01-01 17:42:55 +0300

    finish slider

--- js/slider.js
+++ js/slider.js
@@ -1,53 +1,17 @@
-
-
-$(document).ready(function (){
-  let position = 0;
-  const slidesToShow = 3;
-  const slidesToScroll = 1;
-
-  const container = $('.slider__container');
-  const track = $('.slider__track');
-  const item = $('.slider__item');
-  const itemCount = $(item.length)
-  const btnPrev = $('.slider__button_prev');
-  const btnNext = $('.slider__button_next');
-  const itemWidth = container.width() / slidesToShow;
-  const movePosition = slidesToScroll * itemWidth;
-
-
-  item.each(function (index, item) {
-      $(item).css({
-          minWidth: itemWidth,
-      });
-  });
-
-
-  btnNext.click(function () {
-      position -= movePosition;
-      setPosition()
-  });
-
-  btnPrev.click(function () {
-
-      position += movePosition;
-      setPosition()
-      checkBtn()
-  });
-
-  const setPosition = () => {
-      track.css({
-          transform: `translateX(${position}px)`
-      });
-      checkBtn()
-  };
-
-  const checkBtn = () => {
-      btnPrev.prop('disabled', position === 0);
-      btnNext.prop('disabled',position <= (itemCount - slidesToShow) * itemWidth);
-  };
-  checkBtn();
-});
-
-
-
-
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:40,
+    dots: false,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
